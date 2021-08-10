b38b73fc88f63c989e8b3920a18d7cc7a1943681
lixiugaung <lixiuguang@shangzhao360.com>
2021-08-10 09:32:36 +0800

    优化1.1

--- src/main/java/com/xiuxiuguang/generator/start/GeneratorStart.java
+++ src/main/java/com/xiuxiuguang/generator/start/GeneratorStart.java
@@ -2,6 +2,8 @@ package com.xiuxiuguang.generator.start;
 
 import org.mybatis.generator.api.ShellRunner;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 /*******************************************************************************
@@ -12,18 +14,25 @@ import org.springframework.stereotype.Component;
  * Date         Author          Modification
  * 2021/7/22     lixiuguang    Create the current class
  *******************************************************************************/
+@Configuration
 public class GeneratorStart {
+
+    @Value("${generatorConfigUrl}")
+    private String generatorConfigUrl;
 
     private static final String configfile = "-configfile";
     private static final String overwrite = "-overwrite";
 
-    public static void generator(String configXmlPath) {
+    public void generator(String configXmlPath) {
         String[] args = new String[]{configfile, configXmlPath, overwrite};
         ShellRunner.main(args);
     }
 
-    public static void main(String[] args) {
-        generator("C:\\LiXiuGuang\\Workspace\\IntelliJIDEA_Workspace\\Git_NiuBi6666_Workspace\\generator\\src\\main\\resources\\config\\manager.xml");
+    @Bean
+    public GeneratorStart init() {
+        GeneratorStart generatorStart = new GeneratorStart();
+        generatorStart.generator(generatorConfigUrl);
+        return generatorStart;
     }
 
 }
